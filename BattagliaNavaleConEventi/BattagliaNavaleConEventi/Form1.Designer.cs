6e2d85ab77268078d32c0d5ea34e9bbed9a56b9a
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2025-11-22 08:45:53 +0100

    inizio

---
+++ BattagliaNavaleConEventi/BattagliaNavaleConEventi/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace BattagliaNavaleConEventi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grd_Playground = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grd_Playground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grd_Playground
+            // 
+            this.grd_Playground.AllowUserToAddRows = false;
+            this.grd_Playground.AllowUserToDeleteRows = false;
+            this.grd_Playground.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd_Playground.ColumnHeadersVisible = false;
+            this.grd_Playground.Location = new System.Drawing.Point(107, 3);
+            this.grd_Playground.Name = "grd_Playground";
+            this.grd_Playground.ReadOnly = true;
+            this.grd_Playground.RowHeadersVisible = false;
+            this.grd_Playground.Size = new System.Drawing.Size(400, 400);
+            this.grd_Playground.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grd_Playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grd_Playground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grd_Playground;
+    }
+}
+
---
+++ BattagliaNavaleConEventi/BattagliaNavaleConEventi/Form1.cs
@@ -0,0 +1,77 @@
+﻿using Battaglia_navale;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattagliaNavaleConEventi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            grd_Playground.RowHeadersVisible = false;
+            grd_Playground.ScrollBars = ScrollBars.None;
+        }
+
+        int[,] GrigliaDiGioco = new int[10, 10];
+        int count;
+        int naviaffondate;
+        int[] dimensionebarche = { 1, 1, 1, 1, 2, 2, 3, 3, 4, 5 };
+        CNave[] flotta = new CNave[10];
+        Random generatore = new Random();
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreazioneCampoDigioco();
+        }
+
+        private void CreazioneCampoDigioco()
+        {
+            grd_Playground.Columns.Clear();
+            grd_Playground.Rows.Clear();
+            grd_Playground.RowTemplate.Height = 40;
+            grd_Playground.ColumnHeadersHeight = 40;
+
+            // Crea 10 colonne larghe 40 px
+            for (int c = 0; c < 10; c++)
+            {
+                var col = new DataGridViewTextBoxColumn();
+                col.Name = $"col{c}";
+                col.Width = 40;
+                grd_Playground.Columns.Add(col);
+            }
+            grd_Playground.Rows.Add(10);
+
+            foreach (DataGridViewRow r in grd_Playground.Rows)
+            {
+                r.Height = 40;
+            }
+
+            for(int c = 0; c< 10; c++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    grd_Playground.Rows[i].Cells[c].Style.BackColor = Color.LightBlue;
+                }
+            }
+
+            int idnave = 1;
+
+            for (int i = 0; i < dimensionebarche.Length; i++)
+            {
+                flotta[i] = new CNave(dimensionebarche[i], generatore, GrigliaDiGioco, idnave);
+                idnave++;
+            }
+
+        }
+    }
+}
